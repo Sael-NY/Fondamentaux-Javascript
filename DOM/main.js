9f9a9ae298a5fd42730d65434d135374d6d514d0
Salim El Hattab <hattab840@hotmail.fr>
2023-06-15 17:29:38 +0200

    Work Thursday 15/06

--- DOM/main.js
+++ DOM/main.js
@@ -43,11 +43,31 @@ function interactInput2() {
 // // EXO 3
 
 
-const textToDisplay3 = document.querySelector('.text-to-display-3')
+const result3 = document.querySelector('.text-to-display-4')
+const inputs3 = document.querySelectorAll('.input-4')
 
 
 
 
+for (let i = 0; i < inputs3.length; i++) {
+    inputs3[i].addEventListener('input', onInputs3Change)
+
+}
+
+
+inputs3[1].addEventListener('input', onInputs3Change)
+
+function onInputs3Change() {
+    let result = getDaysBetweenDates(inputs3[1].value, inputs3[0].value)
+    result3.textContent = `Le séjour durera ${result} jours.`;
+}
+
+function getDaysBetweenDates(date2, date1) {
+    let d2 = new Date(date2)
+    let d1 = new Date(date1)
+    let diffTime = d2 - d1;
+    return (diffTime / (1000 * 60 * 60 * 24))
+}
 
 
 // EXO 4
@@ -80,4 +100,61 @@ function onSlider5Change(e) {
 
 
 const result6 = document.querySelector('.text-to-display-6')
-const select = document.querySelector('select-6')
+const select6 = document.querySelector('.select-6')
+
+select6.addEventListener('change', onChangeSelect6)
+
+function onChangeSelect6(event) {
+    result6.textContent = event.currentTarget.value
+}
+
+
+// EXO 7
+
+const result7 = document.querySelector('.div-to-color-7')
+const input7 = document.querySelector('.input-7')
+
+input7.addEventListener('input', onColorClick)
+
+function onColorClick(event) {
+    result7.style.backgroundColor = event.target.value
+}
+
+// EXO 8
+
+const result8 = document.querySelector('.div-to-color-8')
+const btns8 = document.querySelectorAll('.btn-8')
+
+for (let i = 0; i < btns8.length; i++) {
+    btns8[i].addEventListener('click', onBtn8Click)
+
+}
+function onBtn8Click(event) {
+    result8.style.backgroundColor = event.target.getAttribute('data-color')
+
+}
+
+
+// EXO 9
+
+const result9 = document.querySelector('.text-to-display-9')
+const checkboxes = document.querySelectorAll('.checkbox')
+
+
+for (let i = 0; i < checkboxes.length; i++) {
+    checkboxes[i].addEventListener('change', onCheckBoxChange)
+}
+// checkboxes[0].addEventListener('change', onCheckBoxChange)
+// checkboxes[1].addEventListener('change', onCheckBoxChange)
+
+function onCheckBoxChange() {
+    let myStr = ''
+    if (checkboxes[0].checked) {
+        myStr += checkboxes[0].value
+    }
+    if (checkboxes[1].checked) {
+        if (myStr) myStr += ' - '
+        myStr += checkboxes[1].value
+    }
+    result9.textContent = myStr
+}
